4e4d45ca79bebf10a34bd5dda99753faf4f73f0a
sincemcmv <fikretsomay@gmail.com>
2017-10-29 22:42:53 +0300

    asdsdfghfdtrhjgfdgfgfg

--- app/src/main/java/com/example/android/test_1/MainActivity.java
+++ app/src/main/java/com/example/android/test_1/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.android.test_1;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,12 +18,36 @@ public class MainActivity extends AppCompatActivity {
 
     public void onClick(View v)
     {
-//        switch (v.getId())
-//        {
-//            case "@+id/button1" :
-//        }
+        TextView v1 = (TextView)findViewById(R.id.history);
+        float f  = 0;
 
-        //sdsdsdsad
+        switch (v.getId())
+        {
+            case R.id.button1 :
+                f = 1;
+                break;
+            case R.id.button2 :
+                break;
+            case R.id.button3 :
+                break;
+            case R.id.button4 :
+                break;
+            case R.id.button5 :
+                break;
+            case R.id.button6 :
+                break;
+            case R.id.button7 :
+                break;
+            case R.id.button8 :
+                break;
+            case R.id.button9 :
+                break;
+            case R.id.buttonz :
+                break;
+        }
+
+        v1.setText(Float.toString(f));
+
 
     }
 }
