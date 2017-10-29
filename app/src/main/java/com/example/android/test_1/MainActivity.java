c5042e203ce7ac859a99596e4e8a60e765c76145
sincemcmv <fikretsomay@gmail.com>
2017-10-29 21:50:38 +0300

    Initial commit

---
+++ app/src/main/java/com/example/android/test_1/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.android.test_1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void onClick(View v)
+    {
+//        switch (v.getId())
+//        {
+//            case "@+id/button1" :
+//        }
+
+    }
+}
